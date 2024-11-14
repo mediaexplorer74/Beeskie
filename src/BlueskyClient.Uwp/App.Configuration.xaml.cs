4fd9a376257cd5c7cedb8dff20edb72d2671567d
Daniel Paulino <dpaulino@users.noreply.github.com>
2024-11-13 18:19:19 -0800

    Initial commit

---
+++ src/BlueskyClient.Uwp/App.Configuration.xaml.cs
@@ -0,0 +1,51 @@
+﻿using BlueskyClient.ViewModels;
+using CommunityToolkit.Diagnostics;
+using CommunityToolkit.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+#nullable enable
+
+namespace BlueskyClient;
+
+partial class App
+{
+    private IServiceProvider? _serviceProvider;
+
+    public static IServiceProvider Services
+    {
+        get
+        {
+            IServiceProvider? serviceProvider = ((App)Current)._serviceProvider;
+
+            if (serviceProvider is null)
+            {
+                ThrowHelper.ThrowInvalidOperationException("The service provider is not initialized");
+            }
+
+            return serviceProvider;
+        }
+    }
+
+
+    /// <summary>
+    /// Builds the <see cref="IServiceProvider"/> instance with the required services.
+    /// </summary>
+    /// <param name="appsettings">The <see cref="IAppSettings"/> instance to use, if available.</param>
+    /// <returns>The resulting service provider.</returns>
+    private static IServiceProvider ConfigureServices()
+    {
+        ServiceCollection collection = new();
+        ConfigureServices(collection);
+
+        IServiceProvider provider = collection.BuildServiceProvider();
+        return provider;
+    }
+
+    /// <summary>
+    /// Configures services used by the app.
+    /// </summary>
+    /// <param name="services">The target <see cref="IServiceCollection"/> instance to register services with.</param>
+    [Transient(typeof(SignInPageViewModel))]
+    private static partial void ConfigureServices(IServiceCollection services);
+}
---
+++ src/BlueskyClient.Uwp/App.xaml.cs
@@ -0,0 +1,91 @@
+﻿using BlueskyClient.Views;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Activation;
+using Windows.ApplicationModel.Core;
+using Windows.ApplicationModel.Resources.Core;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+#nullable enable
+
+namespace BlueskyClient;
+
+/// <summary>
+/// Provides application-specific behavior to supplement the default Application class.
+/// </summary>
+sealed partial class App : Application
+{
+    private static Frame? AppFrame;
+
+    public static bool IsRightToLeftLanguage
+    {
+        get
+        {
+            string flowDirectionSetting = ResourceContext.GetForCurrentView().QualifierValues["LayoutDirection"];
+            return flowDirectionSetting == "RTL";
+        }
+    }
+
+    public App()
+    {
+        this.InitializeComponent();
+    }
+
+    protected override async void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        await ActivateAsync(args);
+    }
+
+    private Task ActivateAsync(LaunchActivatedEventArgs args)
+    {
+        _serviceProvider = ConfigureServices();
+
+        if (Window.Current.Content is not Frame rootFrame)
+        {
+            rootFrame = new();
+            Window.Current.Content = rootFrame;
+        }
+
+        AppFrame = rootFrame;
+
+        if (args.PrelaunchActivated is false)
+        {
+            CoreApplication.EnablePrelaunch(true);
+
+            if (rootFrame.Content is null)
+            {
+                rootFrame.Navigate(typeof(SignInPage));
+            }
+
+            Window.Current.Activate();
+            ConfigureUI();
+        }
+
+        return Task.CompletedTask;
+    }
+
+    private void ConfigureUI()
+    {
+        if (AppFrame is { } rootFrame)
+        {
+            if (IsRightToLeftLanguage)
+            {
+                rootFrame.FlowDirection = FlowDirection.RightToLeft;
+            }
+
+            CustomizeTitleBar(darkTheme: rootFrame.ActualTheme is ElementTheme.Dark);
+        }
+    }
+
+    private void CustomizeTitleBar(bool darkTheme)
+    {
+        CoreApplication.GetCurrentView().TitleBar.ExtendViewIntoTitleBar = true;
+
+        var viewTitleBar = ApplicationView.GetForCurrentView().TitleBar;
+        viewTitleBar.ButtonBackgroundColor = Colors.Transparent;
+        viewTitleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+        viewTitleBar.ButtonForegroundColor = darkTheme ? Colors.LightGray : Colors.Black;
+    }
+}
