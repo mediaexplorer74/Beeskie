afc3cd7957393f9daed0045719335786ab4cbbf7
Daniel Paulino <dpaulino@users.noreply.github.com>
2024-11-18 22:53:15 -0800

    Added embeds in quote posts

--- src/BlueskyClient.Uwp/Controls/EmbeddedPost.xaml.cs
+++ src/BlueskyClient.Uwp/Controls/EmbeddedPost.xaml.cs
@@ -1,19 +1,7 @@
 ﻿using Bluesky.NET.Models;
 using BlueskyClient.Extensions;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
 #nullable enable
 
@@ -53,4 +41,6 @@ public sealed partial class EmbeddedPost : UserControl
     public string DisplayName => Record?.Author?.DisplayName ?? string.Empty;
 
     public string PostText => Record?.Value?.Text ?? string.Empty;
+
+    public PostEmbed? PostEmbed => Record?.Embeds is [PostEmbed first, ..] ? first : null;
 }
